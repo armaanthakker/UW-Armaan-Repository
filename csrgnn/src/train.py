d2a6a3df0fbde837827e95a462db93e7a8200b7e
Sijin Huang <huangsj96@hotmail.com>
2022-02-01 01:30:10 -0800

    update training process

--- csrgnn/src/train.py
+++ csrgnn/src/train.py
@@ -14,10 +14,13 @@ def forward(model, loader, device, writer, epoch, optimizer=None, train_flag=Tru
         tag = 'test'
 
     # to collect per batch accuracy, precision, recall, f1score, auc
-    accuracy, precision, recall, f1score, auc = [], [], [], [], []
+    # accuracy, precision, recall, f1score, auc = [], [], [], [], []
     total_loss = 0.0
     total_examples = 0.0
     updates_per_epoch = len(loader)
+
+    y_test_all = []
+    y_pred_all = []
 
     for i, batch in enumerate(tqdm(loader, desc=f'epoch{epoch}')):
         if train_flag:
@@ -38,22 +41,32 @@ def forward(model, loader, device, writer, epoch, optimizer=None, train_flag=Tru
         y_pred = torch.round(y_prob_tensor).detach().cpu().numpy()
         y_test = targets.detach().cpu().numpy()
 
+        y_test_all.extend(y_test.tolist())  # OOM???
+        y_pred_all.extend(y_pred.tolist())
+
         total_loss += loss.item() * batch.num_graphs
         total_examples += batch.num_graphs
-        accuracy.append(accuracy_score(y_test, y_pred))
-        precision.append(precision_score(y_test, y_pred))
-        recall.append(recall_score(y_test, y_pred))
-        f1score.append(f1_score(y_test, y_pred))
-        try:
-            auc.append(roc_auc_score(y_test, y_prob))
-        except ValueError:
-            pass
+        # accuracy.append(accuracy_score(y_test, y_pred))
+        # precision.append(precision_score(y_test, y_pred))
+        # recall.append(recall_score(y_test, y_pred))
+        # f1score.append(f1_score(y_test, y_pred))
+        # try:
+        #     auc.append(roc_auc_score(y_test, y_prob))
+        # except ValueError:
+        #     pass
 
-    mean_accuracy = np.mean(accuracy)
-    mean_precision = np.mean(precision)
-    mean_recall = np.mean(recall)
-    mean_f1score = np.mean(f1score)
-    mean_auc = np.mean(auc) 
+
+    # mean_accuracy = np.mean(accuracy)
+    # mean_precision = np.mean(precision)
+    # mean_recall = np.mean(recall)
+    # mean_f1score = np.mean(f1score)
+    # mean_auc = np.mean(auc) 
+
+    mean_accuracy = accuracy_score(y_test_all, y_pred_all)
+    mean_precision = precision_score(y_test_all, y_pred_all)
+    mean_recall = recall_score(y_test_all, y_pred_all)
+    mean_f1score = f1_score(y_test_all, y_pred_all)
+    mean_auc = roc_auc_score(y_test_all, y_pred_all)
      
     writer.add_scalar('loss/{}_loss'.format(tag), total_loss/total_examples, epoch)
     writer.add_scalar('{}/mean_accuracy'.format(tag), mean_accuracy, epoch)
