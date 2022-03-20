699a3295a11a74e0c3adaa1ab50064126d859548
Sijin Huang <huangsj96@hotmail.com>
2022-03-20 16:51:54 +0000

    change grid search

--- csrgnn/src/grid_search.sh
+++ csrgnn/src/grid_search.sh
@@ -13,7 +13,7 @@ for fold in {1..5}; do
     for pred_wind in 48 24 12 6 3; do
         for add_trend in "--add_trends" ""; do
             for no_imputation in "--no_imputation" ""; do
-                DATA_ARGS="--observe_window 12 --predict_window $pred_wind --fold $fold --nrs nds $add_trend"
+                DATA_ARGS="--observe_window 12 --predict_window $pred_wind --fold $fold --nrs nds $add_trend $no_imputation"
                 python main.py $DATA_ARGS --only_preprocess
                 for num_layers in 2 4 6; do
                     TRAIN_ARGS="$DATA_ARGS --num_layers $num_layers"
