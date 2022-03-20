6b23e99fd79271ea8dd0017c33a2eeb5187cb0b0
Sijin Huang <huangsj96@hotmail.com>
2022-03-20 03:04:53 -0700

    update grid search script

--- csrgnn/src/grid_search.sh
+++ csrgnn/src/grid_search.sh
@@ -3,22 +3,26 @@
  # @Author: SijinHuang
  # @Date: 2022-03-12 08:01:51
  # @LastEditors: SijinHuang
- # @LastEditTime: 2022-03-13 09:21:04
+ # @LastEditTime: 2022-03-20 03:04:29
 ### 
 echo "Remember to comment out certain parameters in config.yml!"
 progress_file="grid_progree.log"
-echo "start grid seatch" > $progress_file
-for i in {1..5}; do
+echo "start grid seatch" >> $progress_file
+for fold in {1..5}; do
     # for pred_wind in 3 6 12 24 48; do
     for pred_wind in 48 24 12 6 3; do
         for add_trend in "--add_trends" ""; do
-            python main.py --observe_window 12 --predict_window $pred_wind --num_layers 2 $add_trend --only_preprocess
-            for num_layers in 2 4 6; do
-                echo "--observe_window 12 --predict_window $pred_wind --num_layers $num_layers $add_trend" >> $progress_file
-                python main.py --observe_window 12 --predict_window $pred_wind --num_layers $num_layers $add_trend --skip_preprocess &
-                sleep 5
+            for no_imputation in "--no_imputation" ""; do
+                DATA_ARGS="--observe_window 12 --predict_window $pred_wind --fold $fold --nrs nds $add_trend"
+                python main.py $DATA_ARGS --only_preprocess
+                for num_layers in 2 4 6; do
+                    TRAIN_ARGS="$DATA_ARGS --num_layers $num_layers"
+                    echo $TRAIN_ARGS >> $progress_file
+                    CUDA_VISIBLE_DEVICES=0 python main.py $TRAIN_ARGS --skip_preprocess &
+                    sleep 5
+                done
+                wait
             done
-            wait
         done
     done
 done
