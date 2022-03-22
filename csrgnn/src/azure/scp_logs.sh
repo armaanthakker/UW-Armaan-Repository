922364b23a8ec4c770fab12352fbe78037091345
Sijin Huang <huangsj96@hotmail.com>
2022-03-22 13:32:40 +0000

    add azure configs

---
+++ csrgnn/src/azure/scp_logs.sh
@@ -0,0 +1,10 @@
+###
+ # @Descripttion: 
+ # @Author: SijinHuang
+ # @Date: 2022-03-22 13:07:47
+ # @LastEditors: SijinHuang
+ # @LastEditTime: 2022-03-22 13:09:00
+### 
+scp -r /home/azureuser/sijin/sepsis/Sepsis-GNN/csrgnn/log/* uwr:/home/sijin/sepsis/Sepsis-GNN/csrgnn/log/
+scp -r /home/azureuser/sijin/sepsis/Sepsis-GNN/csrgnn/log_csv/* uwr:/home/sijin/sepsis/Sepsis-GNN/csrgnn/log_csv/
+scp -r /home/azureuser/sijin/sepsis/Sepsis-GNN/csrgnn/log_configs/* uwr:/home/sijin/sepsis/Sepsis-GNN/csrgnn/log_configs/
