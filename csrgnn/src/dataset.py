dfe462bc531ad57ddcd044ce908628e01d9c51c4
Sijin Huang <huangsj96@hotmail.com>
2022-04-23 09:45:12 -0700

    extract method from PYG for multi processing (have bug)

--- csrgnn/src/dataset.py
+++ csrgnn/src/dataset.py
@@ -1,5 +1,8 @@
+import imp
 import pickle
+from sympy import im
 from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 from rich import print
 import shutil
 import torch
@@ -7,6 +10,7 @@ from torch.nn.utils.rnn import pad_sequence
 from collections import Counter
 import numpy as np
 from torch_geometric.data import InMemoryDataset, Data
+from multiprocessing import Pool
 
 PADDED_LENGTH = 25
 
@@ -44,83 +48,108 @@ class MultiSessionsGraph(InMemoryDataset):
         super(MultiSessionsGraph, self)._process()
     
     def process(self):
-        data = pickle.load(open(self.raw_dir + '/' + self.raw_file_names[0], 'rb'))
-        data_list = []
-        
-        for user, sequence, cue_l, y_l in (zip(tqdm(data[0]), data[1], data[2], data[3])):
-            count = Counter([item for itemset in sequence for item in itemset])
-            i = 0
-            nodes = {}    # dict{15: 0, 16: 1, 18: 2, ...}  # ReId nodes for each session graph
-            senders = []  # same as `sequence_t[1:]`
-            sequence_t = [torch.tensor([0] * PADDED_LENGTH, dtype=torch.long)]  # list of item sets. Each element is a set of ReIded node sharing same timestamp. First 0s for padding, deleted later.
-            itemset_len = []  # length of each itemset
-            itemset_len_simplify = [len(itemset) for itemset in sequence]
-            x = []  # unique nodes in temporal order. Origin node ID.
-            for itemset in sequence:
-                temp = [] # set of nodes in same timestamp, ReId
-                if itemset != []:
-                    for node in itemset:
-                        if node not in nodes:
-                            nodes[node] = i
-                            x.append([node])
-                            i += 1
-                        temp.append(nodes[node])
-                    assert len(temp) == len(itemset)
-                    senders.append(temp)
-                    itemset_len.append(len(temp))
-                    sequence_t.append(torch.tensor(temp, dtype=torch.long))
-            assert itemset_len_simplify == itemset_len
+        data_list = load_sequences_and_gen_graphs(self.raw_dir + '/' + self.raw_file_names[0])
 
-            sequence = pad_sequence(sequence_t, batch_first=True, padding_value=i)[1:]
-            # # XXX 是否删除timestamp太少的序列？
-            # # exclude sessions with length of 1 or less
-            # if len(sequence) < 2:
-            #     print(user, sequence) 
-            #     continue
-            receivers = senders[1:] # the first itemset is not a receiver
-            senders = senders[:-1] # the last itemset is not a sender
-            #num_count = [count[i[0]] for i in x]
-
-            pair = {}  # occurrence of the edge
-            send_idx, receive_idx = [], [] # to contain the unique node pairs of senders and receivers  # ReIded sender and receiver for all unique edges
-            send_flatten, receive_flatten = [], [] # flattened version of senders and receivers  # ReIded sender and receiver for all edges including duplications
-
-            for sender_list, receiver_list in zip(senders, receivers):
-                for sender in sender_list:
-                    for receiver in receiver_list:
-                        if str(sender) + '-' + str(receiver) in pair:
-                            pair[str(sender) + '-' + str(receiver)] += 1
-                        else:
-                            pair[str(sender) + '-' + str(receiver)] = 1
-                            send_idx.append(sender)
-                            receive_idx.append(receiver)
-                        send_flatten.append(sender)
-                        receive_flatten.append(receiver)
-
-            edge_index = torch.tensor([send_idx, receive_idx], dtype=torch.long)
-            edge_count = [pair[str(send_idx[i]) + '-' + str(receive_idx[i])] for i in range(len(send_idx))]
-            edge_count = torch.tensor(edge_count, dtype=torch.float)  # occurrence of each unique edge
-
-            count = Counter(send_flatten)
-            out_degree_inv = list(np.array(edge_count)/np.array([count[i] for i in send_idx]))
-
-            count = Counter(receive_flatten)
-            in_degree_inv = list(np.array(edge_count)/np.array([count[i] for i in receive_idx]))
-
-            in_degree_inv = torch.tensor(in_degree_inv, dtype=torch.float)
-            out_degree_inv = torch.tensor(out_degree_inv, dtype=torch.float)
-
-            x = torch.tensor(x, dtype=torch.long) - 67
-            #num_count = torch.tensor(num_count, dtype=torch.float)
-            itemset_len = torch.tensor(itemset_len, dtype=torch.long)
-            sequence_len = torch.tensor([len(sequence)], dtype=torch.long)
-            for i in range(len(cue_l)):
-                cue = torch.tensor([cue_l[i]], dtype=torch.long) - 67
-                y = torch.tensor([y_l[i]], dtype=torch.long)
-                session_graph = Data(user=user, x=x, cue=cue, y=y, edge_index=edge_index,
-                                     sequence=sequence, itemset_len=itemset_len, sequence_len=sequence_len,
-                                     in_degree_inv=in_degree_inv, out_degree_inv=out_degree_inv)
-                data_list.append(session_graph)
- 
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
+
+
+def load_sequences_and_gen_graphs(sequence_fn: str):
+    # print(sequence_fn)
+    data = pickle.load(open(sequence_fn, 'rb'))
+        
+    data_list = []
+    # for user, sequence, cue_l, y_l in (zip(tqdm(data[0]), data[1], data[2], data[3])):
+    #     _data_list = generate_session_graph(user, sequence, cue_l, y_l)
+    #     data_list.extend(_data_list)
+    params = list(zip(data[0], data[1], data[2], data[3]))  # [(user, sequence, cue_l, y_l)]
+    results = [_generate_session_graph(p) for p in tqdm(params)]
+    # 不知道为什么多进程会报错
+    # results = process_map(_generate_session_graph, params, max_workers=8, chunksize=8)
+    # with Pool(processes=8) as pool:
+    #     results = pool.map(_generate_session_graph, params, 8)
+    for _data_list in results:
+        data_list.extend(_data_list)
+    return data_list
+
+
+def _generate_session_graph(params):
+    return generate_session_graph(*params)
+
+
+def generate_session_graph(user, sequence, cue_l, y_l):
+    data_list = []
+    count = Counter([item for itemset in sequence for item in itemset])
+    i = 0
+    nodes = {}    # dict{15: 0, 16: 1, 18: 2, ...}  # ReId nodes for each session graph
+    senders = []  # same as `sequence_t[1:]`
+    sequence_t = [torch.tensor([0] * PADDED_LENGTH, dtype=torch.long)]  # list of item sets. Each element is a set of ReIded node sharing same timestamp. First 0s for padding, deleted later.
+    itemset_len = []  # length of each itemset
+    itemset_len_simplify = [len(itemset) for itemset in sequence]
+    x = []  # unique nodes in temporal order. Origin node ID.
+    for itemset in sequence:
+        temp = [] # set of nodes in same timestamp, ReId
+        if itemset != []:
+            for node in itemset:
+                if node not in nodes:
+                    nodes[node] = i
+                    x.append([node])
+                    i += 1
+                temp.append(nodes[node])
+            assert len(temp) == len(itemset)
+            senders.append(temp)
+            itemset_len.append(len(temp))
+            sequence_t.append(torch.tensor(temp, dtype=torch.long))
+    assert itemset_len_simplify == itemset_len
+
+    sequence = pad_sequence(sequence_t, batch_first=True, padding_value=i)[1:]
+        # # XXX 是否删除timestamp太少的序列？
+        # # exclude sessions with length of 1 or less
+        # if len(sequence) < 2:
+        #     print(user, sequence) 
+        #     continue
+    receivers = senders[1:] # the first itemset is not a receiver
+    senders = senders[:-1] # the last itemset is not a sender
+        #num_count = [count[i[0]] for i in x]
+
+    pair = {}  # occurrence of the edge
+    send_idx, receive_idx = [], [] # to contain the unique node pairs of senders and receivers  # ReIded sender and receiver for all unique edges
+    send_flatten, receive_flatten = [], [] # flattened version of senders and receivers  # ReIded sender and receiver for all edges including duplications
+
+    for sender_list, receiver_list in zip(senders, receivers):
+        for sender in sender_list:
+            for receiver in receiver_list:
+                if str(sender) + '-' + str(receiver) in pair:
+                    pair[str(sender) + '-' + str(receiver)] += 1
+                else:
+                    pair[str(sender) + '-' + str(receiver)] = 1
+                    send_idx.append(sender)
+                    receive_idx.append(receiver)
+                send_flatten.append(sender)
+                receive_flatten.append(receiver)
+
+    edge_index = torch.tensor([send_idx, receive_idx], dtype=torch.long)
+    edge_count = [pair[str(send_idx[i]) + '-' + str(receive_idx[i])] for i in range(len(send_idx))]
+    edge_count = torch.tensor(edge_count, dtype=torch.float)  # occurrence of each unique edge
+
+    count = Counter(send_flatten)
+    out_degree_inv = list(np.array(edge_count)/np.array([count[i] for i in send_idx]))
+
+    count = Counter(receive_flatten)
+    in_degree_inv = list(np.array(edge_count)/np.array([count[i] for i in receive_idx]))
+
+    in_degree_inv = torch.tensor(in_degree_inv, dtype=torch.float)
+    out_degree_inv = torch.tensor(out_degree_inv, dtype=torch.float)
+
+    x = torch.tensor(x, dtype=torch.long) - 67
+        #num_count = torch.tensor(num_count, dtype=torch.float)
+    itemset_len = torch.tensor(itemset_len, dtype=torch.long)
+    sequence_len = torch.tensor([len(sequence)], dtype=torch.long)
+    for i in range(len(cue_l)):
+        cue = torch.tensor([cue_l[i]], dtype=torch.long) - 67
+        y = torch.tensor([y_l[i]], dtype=torch.long)
+        session_graph = Data(user=user, x=x, cue=cue, y=y, edge_index=edge_index,
+                                    sequence=sequence, itemset_len=itemset_len, sequence_len=sequence_len,
+                                    in_degree_inv=in_degree_inv, out_degree_inv=out_degree_inv)
+        data_list.append(session_graph)
+    return data_list
